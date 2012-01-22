076c2109a2d3f352124e54d96808d330ff431cc3
Ilya Obshadko <xfyre@xfyre.com>
2012-01-22 19:31:39 +0300

    % in progress

---
+++ ElectionsWatcher/WatcherChecklistScreenCell.h
@@ -0,0 +1,13 @@
+//
+//  WatcherChecklistScreenCell.h
+//  ElectionsWatcher
+//
+//  Created by xfire on 22.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WatcherChecklistScreenCell : UITableViewCell
+
+@end
---
+++ ElectionsWatcher/WatcherChecklistScreenCell.m
@@ -0,0 +1,29 @@
+//
+//  WatcherChecklistScreenCell.m
+//  ElectionsWatcher
+//
+//  Created by xfire on 22.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "WatcherChecklistScreenCell.h"
+
+@implementation WatcherChecklistScreenCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
